b135617a8a4d01fd993f42b5b18b36dcdddb142c
petrikuittinen <petri.kuittinen@hamk.fi>
2025-09-04 16:43:49 +0300

    recent changes to openai API

--- openai_examples/openai_openrouter_streaming.py
+++ openai_examples/openai_openrouter_streaming.py
@@ -8,16 +8,18 @@ stream = client.chat.completions.create(
     model="moonshotai/kimi-k2:free",
     messages=[
         {
+            "role": "system", 
+            "content": "Answer directly without a preamble."
+        },
+        {
             "role": "user",
             "content": "List TOP10 most widely spoken languages.",
         },
     ],
+    temperature=0.3,
     stream=True,
 )
 
 for chunk in stream:
     if chunk.choices[0].delta.content:
         print(chunk.choices[0].delta.content, end="", flush=True)
-    #print(chunk)
-    #print(chunk.choices[0].delta.content)
-    #print("****************")
